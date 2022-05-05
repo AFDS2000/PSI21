e8e46dfce1eb1b77bd72afe7b61ec9de01c79440
fc53475 <fc53475@alunos.fc.ul.pt>
2022-05-05 16:15:26 +0100

    label endDate adicionada

--- frontend/src/app/components/criar-projeto/criar-projeto.component.ts
+++ frontend/src/app/components/criar-projeto/criar-projeto.component.ts
@@ -36,6 +36,10 @@ export class CriarProjetoComponent implements OnInit {
        
         startDate: new FormControl("", [
             Validators.required,
+        ]),
+
+        endDate: new FormControl("", [
+           
         ])
     });
 }
