cb3b3861918b591859a149f362d5c24b2d066763
André Seixas <andrefil.seixas@gmail.com>
2022-05-06 00:53:19 +0100

    Por fazer schedule

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -13,13 +13,15 @@ import { TaskComponent } from './components/task/task.component';
 import { HomeComponent } from './components/home/home.component';
 import { MyTasksComponent } from './components/my-tasks/my-tasks.component';
 import { LoginGuardService } from './services/login-guard.service';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home/myTasks' },
     { path: 'login', component: LoginComponent, canActivate: [LoginGuardService]},
-    { path: 'home', component: HomeComponent , canActivate: [AuthGuardService], children: [{
-        path: 'myTasks', component: MyTasksComponent, canActivate: [AuthGuardService]
-    }]},
+    { path: 'home', component: HomeComponent , canActivate: [AuthGuardService], children: [
+        { path: 'myTasks', component: MyTasksComponent, canActivate: [AuthGuardService]},
+        { path: 'mySchedule', component: ScheduleComponent, canActivate: [AuthGuardService]}
+    ]},
     { path: 'criarEquipa', component: CreateTeamComponent, canActivate: [AuthGuardService, AdminGuardService] },
     { path: 'criarUtilizador', component: SignupComponent, canActivate: [AuthGuardService, AdminGuardService] },
     { path: 'tasks', component: TaskComponent, canActivate: [AuthGuardService] },
